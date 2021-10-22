c6a3b3d49c19a5fdf73d2fd8e365d437f244dfed
Frederik <frederik.geth@gmail.com>
2021-10-22 20:55:41 +1100

    PDF compilation is working!

--- docs/make.jl
+++ docs/make.jl
@@ -1,32 +1,44 @@
 push!(LOAD_PATH,"../src/")
 using Documenter, StochasticPowerModels
 
-makedocs(sitename="StochasticPowerModels Documentation")
+
+
 
 # A flag to check if we are running in a GitHub action.
 const _IS_GITHUB_ACTIONS = get(ENV, "GITHUB_ACTIONS", "false") == "true"
 
 # Pass --pdf to build the PDF. On GitHub actions, we always build the PDF.
-const _PDF = findfirst(isequal("--pdf"), ARGS) !== nothing || _IS_GITHUB_ACTIONS
-# const _PDF = true
+# const _PDF = findfirst(isequal("--pdf"), ARGS) !== nothing || _IS_GITHUB_ACTIONS
+const _PDF = true
+
+const _PAGES = [
+    "Home" => "index.md",
+    "Manual" => [
+        "Getting Started" => "quickguide.md",
+        "Mathematical Model" => "math-model.md",
+    ],
+    "Library" => [
+        "Network Formulations" => "formulations.md",
+    ]
+];
+
+@time Documenter.makedocs(
+    modules = [StochasticPowerModels],
+    format = Documenter.HTML(mathengine = Documenter.MathJax()),
+    sitename = "StochasticPowerModels",
+    authors = "Tom Van Acker and contributors.",
+    pages = _PAGES,
+    )
 
 if _PDF
-    latex_platform = _IS_GITHUB_ACTIONS ? "docker" : "native"
+    # latex_platform = _IS_GITHUB_ACTIONS ? "docker" : "native"
+    latex_platform = "docker"
     @time Documenter.makedocs(
-        modules = [StochasticPowerModels],
-        format = Documenter.HTML(mathengine = Documenter.MathJax()),
         sitename = "StochasticPowerModels",
         authors = "Tom Van Acker and contributors.",
-        pages = [
-            "Home" => "index.md",
-            "Manual" => [
-                "Getting Started" => "quickguide.md",
-                "Mathematical Model" => "math-model.md",
-            ],
-            "Library" => [
-                "Network Formulations" => "formulations.md",
-            ],
-        ]
+        format = Documenter.LaTeX(platform = latex_platform),
+        build = "latex_build",
+        pages = _PAGES,
     )
     # Hack for deploying: copy the pdf (and only the PDF) into the HTML build
     # directory! We don't want to copy everything in `latex_build` because it
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,23 +1,23 @@
-# HarmonicPowerModels.jl Documentation
+# StochasticPowerModels.jl Documentation
 
 ```@meta
-CurrentModule = HarmonicPowerModels
+CurrentModule = StochasticPowerModels
 ```
 
 ## Overview
 
-HarmonicPowerModels.jl is a research-grade Julia/JuMP package for experimentation with Steady-State Power Network Optimization with Harmonics, extending PowerModels.jl.
+StochasticPowerModels.jl is a research-grade Julia/JuMP package for experimentation with Steady-State Power Network Optimization under uncertainty, extending PowerModels.jl.
 
 
 ## Installation
 
-For now, HarmonicPowerModels is unregistered. Nevertheless, you can install it through
+For now, StochasticPowerModels is unregistered. Nevertheless, you can install it through
 
 ```
-] add https://github.com/timmyfaraday/HarmonicPowerModels.jl.git
+] add https://github.com/timmyfaraday/StochasticPowerModels.jl.git
 ```
 
-At least one solver is required for running HarmonicPowerModels.  The open-source solver Ipopt is recommended, as it is fast, scaleable and can be used to solve a wide variety of the problems and network formulations provided in PowerModels.  The Ipopt solver can be installed via the package manager with
+At least one solver is required for running StochasticPowerModels.  The open-source solver Ipopt is recommended, as it is fast, scaleable and can be used to solve a wide variety of the problems and network formulations provided in PowerModels.  The Ipopt solver can be installed via the package manager with
 
 ```julia
 ] add Ipopt
@@ -26,5 +26,5 @@ At least one solver is required for running HarmonicPowerModels.  The open-sourc
 Test that the package works by running
 
 ```julia
-] test HarmonicPowerModels
+] test StochasticPowerModels
 ```
